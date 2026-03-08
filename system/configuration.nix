13dbaa386fa8220d04602c9eb805f5c81bd87d35
ultexplorer <ultexplorer@gmail.com>
2026-03-08 10:42:09 +0000

    update boot.loader limit = 5

--- system/configuration.nix
+++ system/configuration.nix
@@ -15,6 +15,7 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 5;
 
   boot.kernelParams = [
     "threadirqs"
