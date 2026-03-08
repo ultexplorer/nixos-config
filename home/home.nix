5a65de35a6fe289b47c36a1e55b952d05b8e80d2
ultexplorer <ultexplorer@gmail.com>
2026-03-08 15:03:36 +0000

    delete home.nix from /

--- home/home.nix
+++ home/home.nix
@@ -18,5 +18,16 @@
   LIBVA_DRIVER_NAME = "radeonsi";
   MOZ_DISABLE_RDD_SANDBOX = "1";
 };
-
+ 
+ home.file.".config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml" = {
+    text = ''
+<?xml version="1.0" encoding="UTF-8"?>
+<channel name="xfwm4" version="1.0">
+  <property name="general" type="empty">
+    <property name="vblank_mode" type="string" value="auto"/>
+  </property>
+</channel>
+'';
+    force = true;
+  };
 }
