b31008e8e1575da196369242a219078cbde6f2ff
ultexplorer <ultexplorer@gmail.com>
2026-03-09 19:59:55 +0000

    fix flake.nix outputs

--- flake.nix
+++ flake.nix
@@ -7,26 +7,27 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, nixos-hardware, home-manager, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; overlays = [ (import ./overlays/default.nix) ]; };
-      in
-      {
-        # Основная конфигурация для твоей машины
-        nixosConfigurations = {
-          t14 = pkgs.lib.nixosSystem {
-            inherit system;
-            modules = [
-              ./hosts/t14/default.nix
-            ];
-            # specialArgs = { inherit inputs; }; <- удаляем или заменяем на self
-            specialArgs = { inherit self; }; # <-- если нужно передавать flake
-          };
+  outputs = { self, nixpkgs, home-manager, flake-utils, ... }:
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; overlays = [ (import ./overlays/default.nix) ]; };
+    in
+    {
+      nixosConfigurations = {
+        t14 = pkgs.lib.nixosSystem {
+          inherit system;
+          modules = [ ./hosts/t14/default.nix ];
         };
+      };
 
-        # DevShell для разработки
-        devShells.${system}.default = import ./devshells/default.nix { inherit pkgs; };
-      }
-    );
+      devShells.${system}.default = import ./devshells/default.nix { inherit pkgs; };
+
+      # <--- Добавляем атрибут home-manager для nix run
+      home-manager = {
+        type = "app";
+        program = "${home-manager}/bin/home-manager";
+      };
+    }
+  );
+  
 }
