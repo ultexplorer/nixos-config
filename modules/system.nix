614aae86fc2a277d50f48c425ce843bbb853adfe
ultexplorer <ultexplorer@gmail.com>
2026-03-11 17:46:12 +0000

    fix system

--- modules/system.nix
+++ modules/system.nix
@@ -2,28 +2,30 @@
 
 {
   # Локализация
-  i18n.defaultLocale = "ru_RU.UTF-8";
+  i18n.defaultLocale = "en_US.UTF-8";
   time.timeZone = "Europe/Berlin"; # Настрой под себя
 
   # Настройка консоли (TTY)
   console = {
     font = "ter-v32n"; 
     packages = [ pkgs.terminus_font ];
-    keyMap = "ru";
+    keyMap = "us";
   };
 
   # Шрифты для системы (GUI)
   fonts.packages = with pkgs; [
     terminus_font
-    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
   ];
 
   # Общие пакеты, которые нужны везде (даже на сервере)
   environment.systemPackages = with pkgs; [
-    git
     vim
+    mc
+    git
     wget
-    htop
     curl
+    htop
+    pciutils # Чтобы смотреть железо через lspci
+    usbutils # Чтобы видеть твою будущую клавиатуру через lsusb
   ];
 }
