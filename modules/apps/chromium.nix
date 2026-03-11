301ed1ca385210a62f5c3eeeb82e64c2918fab39
ultexplorer <ultexplorer@gmail.com>
2026-03-11 22:40:20 +0100

    add encoding

--- modules/apps/chromium.nix
+++ modules/apps/chromium.nix
@@ -7,12 +7,12 @@
       # Теперь эта опция будет видна, так как она внутри Home Manager
       commandLineArgs = [
         "--ignore-gpu-blocklist"
+        "--ozone-platform-hint=auto"
+        "--enable-features=VaapiVideoDecoder,VaapiVideoEncoder" # Добавили Encoder
         "--enable-gpu-rasterization"
         "--enable-zero-copy"
-        "--enable-features=VaapiVideoDecodeLinuxGL"
-        "--enable-features=CanvasOopRasterization"
-        "--ozone-platform-hint=auto"
       ];
+
       extensions = [
         { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # uBlock Origin (формат HM чуть другой)
       ];
