2b205b1efeb830da675b787e7969a362df69c29c
ultexplorer <ultexplorer@gmail.com>
2026-03-11 20:45:04 +0100

    fix t14/default.nix

--- hosts/t14/default.nix
+++ hosts/t14/default.nix
@@ -6,6 +6,7 @@
     ../../modules/system.nix           # База (шрифты, консоль, git)
     ../../modules/desktop/common.nix # Общее для GUI (звук, видео)
     ../../modules/desktop/xfce.nix   # Конкретно оболочка XFCE
+    ../../modules/users/me.nix
     # Если у тебя AMD версия T14:
     inputs.nixos-hardware.nixosModules.lenovo-thinkpad-t14-amd-gen2 # Вот этот модуль специально для тебя
   ];
@@ -32,10 +33,18 @@
     enable = true;
     enable32Bit = true;
     extraPackages = with pkgs; [
-      amdvlk # Драйвер Vulkan от AMD
+     # amdvlk # Драйвер Vulkan от AMD
       vaapiVdpau
       libvdpau-va-gl
     ];
   };
+
+  # Настройки TLP для батареи ThinkPad
+  services.tlp.enable = true;
+  services.tlp.settings = {
+    START_CHARGE_THRESH_BAT0 = 75;
+    STOP_CHARGE_THRESH_BAT0 = 80;
+  };
+  
 
 }
