242355f10928250eb5056caa92479d7271436cc6
ultexplorer <ultexplorer@gmail.com>
2026-03-11 17:31:19 +0000

    fix xfce.nix

--- modules/desktop/xfce.nix
+++ modules/desktop/xfce.nix
@@ -4,9 +4,6 @@
   # Включаем XFCE
   services.xserver.desktopManager.xfce.enable = true;
 
-  # Включаем sddm для выбора среды
-  services.xserver.displayManager.sddm.enable = true;
-
   # XFCE специфические пакеты
   environment.systemPackages = with pkgs; [
     xfce4
