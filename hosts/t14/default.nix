96f3ccce26e2c319a1defede80fa93137b43a8a6
ultexplorer <ultexplorer@gmail.com>
2026-03-16 22:33:28 +0100

    fix

--- hosts/t14/default.nix
+++ hosts/t14/default.nix
@@ -29,8 +29,7 @@
     settings = {
       default_session = {
         command = ''
-          ${pkgs.greetd.tuigreet}/bin/tuigreet \
-         --remember \
+          ${pkgs.greetd.tuigreet}/bin/tuigreet 
         '';
         user = "greeter";
       };
